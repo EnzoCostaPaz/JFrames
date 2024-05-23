48bb579f39317798e3b98587b631e73f60637efa
EnzoCostaPaz <133404019+EnzoCostaPaz@users.noreply.github.com>
2024-05-22 21:37:57 -0300

    adicionado ComboBox

---
+++ ComboBoxRadio/src/RodarComboBoxRadio.java
@@ -0,0 +1,8 @@
+import javax.swing.JFrame;
+
+public class RodarComboBoxRadio {
+    public static void main(String[] args) {
+        ComboBoxRadio app = new ComboBoxRadio();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
